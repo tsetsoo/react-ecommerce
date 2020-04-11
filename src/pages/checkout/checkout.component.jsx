f81d2c7ed5656509d48d5e60df405b1770a941ad
i333859 <tsvetelin.pantev@sap.com>
2020-04-11 11:50:38 +0300

    stripe integration and favicon

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,6 +7,7 @@ import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
 
 import './checkout.styles.scss'
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component'
 
 const CheckoutPage = ({ cartItems, total }) => {
     return (
@@ -35,6 +36,12 @@ const CheckoutPage = ({ cartItems, total }) => {
             <div className='total'>
                 <span>${total}</span>
             </div>
+            <div className='test-warning'>
+                *Please use the following test credit card for payments*
+                <br />
+                4242 4242 4242 4242 - Exp: 01/23 - CVC: 123
+            </div>
+            <StripeCheckoutButton price={total}/>
         </div>
     )
 }
