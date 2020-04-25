214f9113852b1e44ec5c85b34e33ead06665a987
i333859 <tsvetelin.pantev@sap.com>
2020-04-25 16:33:54 +0300

    server code for prod

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app = express()
 const port = process.env.PORT || 3001
 
-app.use(compression());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cors())
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 if(process.env.NODE_ENV === 'production') {
+    app.use(compression());
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')))
 
     app.get('*', (req, res) => {
